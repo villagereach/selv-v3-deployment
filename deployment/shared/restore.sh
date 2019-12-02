3d35d73f32c61eb898dae97913e4a260483caf74
saleksandra <soltys.ola@gmail.com>
2019-12-02 15:05:38 +0100

    SELV3-8: Changed restore database file

--- deployment/shared/restore.sh
+++ deployment/shared/restore.sh
@@ -3,17 +3,17 @@
 if [ -f backup_file.sql.zip ]
 then
     unzip -o backup_file.sql.zip
-    /usr/local/bin/docker-compose down
+    sudo docker-compose down
 
-    /usr/local/bin/docker start $POSTGRES_CONTAINER_NAME
+    sudo docker start $POSTGRES_CONTAINER_NAME
 
-    /usr/local/bin/docker exec -i $POSTGRES_CONTAINER_NAME psql -U postgres -c 'DROP DATABASE ${DATABASE_NAME};'
-    /usr/local/bin/docker exec -i $POSTGRES_CONTAINER_NAME psql -U postgres -c 'CREATE DATABASE ${DATABASE_NAME};'
+    sudo docker exec -i $POSTGRES_CONTAINER_NAME psql -U postgres -c 'DROP DATABASE ${DATABASE_NAME};'
+    sudo docker exec -i $POSTGRES_CONTAINER_NAME psql -U postgres -c 'CREATE DATABASE ${DATABASE_NAME};'
 
-    cat backup_file.sql | /usr/local/bin/docker exec -i $POSTGRES_CONTAINER_NAME psql -U postgres -d $DATABASE_NAME
+    cat backup_file.sql | sudo docker exec -i $POSTGRES_CONTAINER_NAME psql -U postgres -d $DATABASE_NAME
 
     export spring_profiles_active="production"
-    /usr/local/bin/docker-compose up --build --force-recreate -d
+    sudo docker-compose up --build --force-recreate -d
 else
     echo "Missing reference backup zip file"
 fi
