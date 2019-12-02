48fe4cc15c3e9d739f012386ba91558a4a8af551
saleksandra <soltys.ola@gmail.com>
2019-12-02 15:19:40 +0100

    SELV3-8: changed reference to variables

--- deployment/shared/restore.sh
+++ deployment/shared/restore.sh
@@ -5,12 +5,12 @@ then
     unzip -o backup_file.sql.zip
     sudo docker-compose down
 
-    sudo docker start $POSTGRES_CONTAINER_NAME
+    sudo docker start ${POSTGRES_CONTAINER_NAME}
 
-    sudo docker exec -i $POSTGRES_CONTAINER_NAME psql -U postgres -c 'DROP DATABASE ${DATABASE_NAME};'
-    sudo docker exec -i $POSTGRES_CONTAINER_NAME psql -U postgres -c 'CREATE DATABASE ${DATABASE_NAME};'
+    sudo docker exec -i ${POSTGRES_CONTAINER_NAME} psql -U postgres -c 'DROP DATABASE ${DATABASE_NAME};'
+    sudo docker exec -i ${POSTGRES_CONTAINER_NAME} psql -U postgres -c 'CREATE DATABASE ${DATABASE_NAME};'
 
-    cat backup_file.sql | sudo docker exec -i $POSTGRES_CONTAINER_NAME psql -U postgres -d $DATABASE_NAME
+    cat backup_file.sql | sudo docker exec -i ${POSTGRES_CONTAINER_NAME} psql -U postgres -d ${DATABASE_NAME}
 
     export spring_profiles_active="production"
     sudo docker-compose up --build --force-recreate -d
