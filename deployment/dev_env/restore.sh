3cb6a92bce0b3affa52ca3153df3616a030f4033
saleksandra <soltys.ola@gmail.com>
2019-12-04 12:58:51 +0100

    SELV3-8: Added container list to logs

--- deployment/dev_env/restore.sh
+++ deployment/dev_env/restore.sh
@@ -5,6 +5,8 @@ export DOCKER_HOST="dev.selv.org.mz:2376"
 export CREDENTIALS_SUB_DIRECTORY="dev_env"
 export DOCKER_CERT_PATH="${PWD}/../../credentials/${CREDENTIALS_SUB_DIRECTORY}"
 
+/usr/local/bin/docker-compose pull
+
 chmod 755 ./restore.sh
 chmod 755 ../shared/restore.sh
 ../shared/restore.sh
--- deployment/shared/restore.sh
+++ deployment/shared/restore.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -f backup_file.sql.zip ]
 then
@@ -12,30 +12,33 @@ then
         exit 0;
     fi
 
-    if [ -n  "$DATABASE_NAME" ]
-    then
-        echo "Setting the default value for database name - ${DATABASE_NAME}"
-        DATABASE_NAME=$DEFAULT_DATABASE_NAME
-    fi
+    # if [ -n  "$DATABASE_NAME" ]
+    # then
+    #     echo "Setting the default value for database name - ${DATABASE_NAME}"
+    #     DATABASE_NAME=$DEFAULT_DATABASE_NAME
+    # fi
 
-    if [ -n  "$POSTGRES_CONTAINER_NAME" ]
-    then
-        echo "Setting the default value for container name - ${POSTGRES_CONTAINER_NAME}"
-        POSTGRES_CONTAINER_NAME=$DEFAULT_POSTGRES_CONTAINER_NAME
-    fi
+    # if [ -n  "$POSTGRES_CONTAINER_NAME" ]
+    # then
+    #     echo "Setting the default value for container name - ${POSTGRES_CONTAINER_NAME}"
+    #     POSTGRES_CONTAINER_NAME=$DEFAULT_POSTGRES_CONTAINER_NAME
+    # fi
 
     unzip backup_file.sql.zip -d tmp_backup && mv tmp_backup/* "backup_file.sql"
     rm -d tmp_backup
     rm -f backup_file.sql.zip
 
-    sudo docker container stop $(sudo docker container ls -aq)
+    echo "Containers list: "
+    docker container ls -aq
+    echo "Docker host - $DOCKER_HOST"
+    docker container stop $(docker container ls -aq)
 
-    sudo docker start $POSTGRES_CONTAINER_NAME
+    docker start $POSTGRES_CONTAINER_NAME
 
-    sudo docker exec -i $POSTGRES_CONTAINER_NAME psql -U postgres -c "DROP DATABASE ${DATABASE_NAME};"
-    sudo docker exec -i $POSTGRES_CONTAINER_NAME psql -U postgres -c "CREATE DATABASE ${DATABASE_NAME};"
+    docker exec -i $POSTGRES_CONTAINER_NAME psql -U postgres -c "DROP DATABASE ${DATABASE_NAME};"
+    docker exec -i $POSTGRES_CONTAINER_NAME psql -U postgres -c "CREATE DATABASE ${DATABASE_NAME};"
 
-    cat backup_file.sql | sudo docker exec -i $POSTGRES_CONTAINER_NAME psql -U postgres -d ${DATABASE_NAME}
+    cat backup_file.sql | docker exec -i $POSTGRES_CONTAINER_NAME psql -U postgres -d ${DATABASE_NAME}
     
     rm -f backup_file.sql
     ../shared/restart.sh
