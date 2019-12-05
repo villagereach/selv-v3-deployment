2420ed103045e8847977f4e7f78392dcf004f863
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-12-05 11:46:13 +0100

    SELV3-19: fixed URL for production reporting stack in deployment script

--- deployment/production_reporting_env/services/deploy_services.sh
+++ deployment/production_reporting_env/services/deploy_services.sh
@@ -2,7 +2,7 @@
 
 export DOCKER_TLS_VERIFY="1"
 export COMPOSE_TLS_VERSION=TLSv1_2
-export DOCKER_HOST="reporting.selv.openlmis.org:2376"
+export DOCKER_HOST="reporting.selv.org.mz:2376"
 export DOCKER_CERT_PATH="${PWD}/../../../credentials/production_reporting_env"
 export DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
 
