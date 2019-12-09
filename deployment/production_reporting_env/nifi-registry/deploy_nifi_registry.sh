f8ba135dfea09451091caefba1c437bcad10f0a0
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-12-09 19:08:55 +0100

    SELV3-19: fixed path to docker certificates for nifi-registry deployment

--- deployment/production_reporting_env/nifi-registry/deploy_nifi_registry.sh
+++ deployment/production_reporting_env/nifi-registry/deploy_nifi_registry.sh
@@ -4,7 +4,7 @@ set -e
 export DOCKER_TLS_VERIFY="1"
 export COMPOSE_TLS_VERSION=TLSv1_2
 export DOCKER_HOST="reporting.selv.org.mz:2376"
-export DOCKER_CERT_PATH="${PWD}/../../../credentials/selv_reporting_env"
+export DOCKER_CERT_PATH="${PWD}/../../../credentials/production_reporting_env"
 
 /usr/local/bin/docker-compose kill
 /usr/local/bin/docker-compose down -v
