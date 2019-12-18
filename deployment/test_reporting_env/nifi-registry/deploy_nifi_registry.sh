d3a3e8926dea4b4c721d83279ac98bef999a8f8d
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-12-18 12:53:34 +0100

    SELV3-19: fixed docker host URL for test reporting instance

--- deployment/test_reporting_env/nifi-registry/deploy_nifi_registry.sh
+++ deployment/test_reporting_env/nifi-registry/deploy_nifi_registry.sh
@@ -3,7 +3,7 @@
 set -e
 export DOCKER_TLS_VERIFY="1"
 export COMPOSE_TLS_VERSION=TLSv1_2
-export DOCKER_HOST="selv-test.reports.openlmis.org:2376"
+export DOCKER_HOST="reporting.selv.org.mz:2376"
 export DOCKER_CERT_PATH="${PWD}/../../../credentials/test_reporting_env"
 
 /usr/local/bin/docker-compose kill
--- deployment/test_reporting_env/services/deploy_services.sh
+++ deployment/test_reporting_env/services/deploy_services.sh
@@ -2,7 +2,7 @@
 
 export DOCKER_TLS_VERIFY="1"
 export COMPOSE_TLS_VERSION=TLSv1_2
-export DOCKER_HOST="selv-test.reports.openlmis.org:2376"
+export DOCKER_HOST="reporting.test.selv.org.mz:2376"
 export DOCKER_CERT_PATH="${PWD}/../../../credentials/test_reporting_env"
 export DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
 
@@ -10,11 +10,7 @@ export REPORTING_DIR_NAME=reporting
 
 distro_repo=$1
 
-# In order to avoid generated new certificates between next deploys of ReportingStack
-# we need to move them to seperate volume marked as external.
-# External volumes are not removed even we use docker-compose down with -v option.
-# The external volume need to be created before the docker start
-docker volume create letsencrypt-config
+docker volume create pgdata
 
 cd "$distro_repo/$REPORTING_DIR_NAME" &&
 $DOCKER_COMPOSE_BIN kill &&
