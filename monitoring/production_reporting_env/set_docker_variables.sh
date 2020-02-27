0d430261d69ae5e3a9f02cdfee994b98abb79a3c
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2020-02-27 17:00:41 +0100

    fixed reporting servers URLs in monitoring section

--- monitoring/production_reporting_env/set_docker_variables.sh
+++ monitoring/production_reporting_env/set_docker_variables.sh
@@ -1,4 +1,4 @@
 export DOCKER_TLS_VERIFY="1"
 export COMPOSE_TLS_VERSION=TLSv1_2
-export DOCKER_HOST="selv.reports.openlmis.org:2376"
+export DOCKER_HOST="reporting.selv.org.mz:2376"
 export DOCKER_CERT_PATH="${PWD}/../../credentials/production_reporting_env"
