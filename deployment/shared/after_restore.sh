b019964b538c8d84bdcd2ee45bc4762f40e3c558
saleksandra <soltys.ola@gmail.com>
2020-04-20 12:37:05 +0200

    SELV3-199: Update redirecturi in after_restore.sh script

--- deployment/shared/after_restore.sh
+++ deployment/shared/after_restore.sh
@@ -10,10 +10,13 @@ export $(grep -v '^#' .env-restore | xargs)
 : ${SERVICE_CLIENT_SECRET:?"Need to set SERVICE_CLIENT_SECRET"}
 : ${CLIENT_SECRET:?"Need to set CLIENT_SECRET"}
 : ${CLIENT_USERNAME:?"Need to set CLIENT_USERNAME"}
+: ${SUPERSET_CLIENT:?"Need to set SUPERSET_CLIENT"}
+: ${SUPERSET_CLIENT_REDIRECT_URI:?"Need to set SUPERSET_CLIENT_REDIRECT_URI"}
 
 sql="UPDATE auth.auth_users SET password = '${ENCODED_USER_PASSWORD}';
 UPDATE notification.user_contact_details SET email = NULL;
 UPDATE auth.oauth_client_details SET clientsecret = '${SERVICE_CLIENT_SECRET}' WHERE clientid = '${SERVICE_CLIENT_ID}';
 UPDATE auth.oauth_client_details SET clientsecret = '${CLIENT_SECRET}' WHERE clientid = '${CLIENT_USERNAME}';"
+UPDATE auth.oauth_client_details SET redirecturi = '${SUPERSET_CLIENT_REDIRECT_URI}' WHERE clientid = '${SUPERSET_CLIENT}';"
 
 docker exec -i $POSTGRES_CONTAINER_NAME psql -U $POSTGRES_USER -d $DATABASE_NAME -c "$sql"
