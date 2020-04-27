3eed6c81757607b9f4b5433c4224223d8158c98b
oskarhinc <oskarhinc@gmail.com>
2020-04-27 13:55:22 +0200

    SELV3-34: Added www prefix to production instance URL

--- deployment/production_env/deploy_to_production_env.sh
+++ deployment/production_env/deploy_to_production_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="selv.org.mz:2376"
+export DOCKER_HOST="www.selv.org.mz:2376"
 export DOCKER_CERT_PATH="${PWD}/../../credentials/production_env"
 
 /usr/local/bin/docker-compose pull
