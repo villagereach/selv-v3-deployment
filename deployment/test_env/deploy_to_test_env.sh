3cbfe8d6fdaea797d8a2cf3e400a6e991206fb2a
Maciej Grochalski <mgrochalski@soldevelo.com>
2026-04-14 14:40:01 +0200

    Modernize test env docker-compose and fix DOCKER_HOST protocol

--- deployment/test_env/deploy_to_test_env.sh
+++ deployment/test_env/deploy_to_test_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="test.selv.org.mz:2376"
+export DOCKER_HOST="tcp://test.selv.org.mz:2376"
 export CREDENTIALS_SUB_DIRECTORY="test_env"
 export DOCKER_CERT_PATH="${PWD}/../../credentials/${CREDENTIALS_SUB_DIRECTORY}"
 
